2957d49d074b775d6bdfcbc465549ef6fad82385
Andrick <andricklopez123@gmail.com>
2024-09-24 13:59:55 -0600

    Primer comando

---
+++ src/main/java/ucr/ac/cr/tm2100/g3/lab06/Lab06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.tm2100.g3.lab06;
+
+/**
+ *
+ * @author lab2-10
+ */
+public class Lab06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
